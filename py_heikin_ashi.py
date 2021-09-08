d6b39db50303211bff34b028a4dcbce8e42cbec5
bahram2000 <50679501+bahram2000@users.noreply.github.com>
2021-09-08 10:45:32 +0430

    Create py_heikin_ashi.py

---
+++ py_heikin_ashi.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+def heiken_ashi(data):
+  res=pd.DataFrame()
+  res["Date"]=data.index
+  O=[0]
+  O.extend(list((data.Close+data.Open)/2)[:-1])
+  res["Open"]=O
+  C=list((data.Close+data.Open+data.High+data.Low)/4)
+  res["High"]=np.array(data.High)
+  res["Low"]=np.array(data.Low)
+  res["Close"]=C
+  return(res)
